09d71e10a0c9bf53dad241e76e94c517a3048d81
BenJohnCarson <benjohncarson@gmail.com>
2017-07-03 12:27:22 +0100

    Implemented dragging text

--- src/js/drag-n-drop.js
+++ src/js/drag-n-drop.js
@@ -3,11 +3,7 @@ function dragstartHandler(event) {
 }
 
 function dropHandler(event){
-    event.preventDefault();
     var data = event.dataTransfer.getData('text');
-    event.target.appendChild(document.getElementById(data));
-}
-
-function dragoverHandler(event){
     event.preventDefault();
+    event.target.value = data;
 }
